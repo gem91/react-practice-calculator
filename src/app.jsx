125f8d9364d56a04a020f9b0fd0798f17d262a2e
mjkim <91gem@naver.com>
2022-05-20 17:22:34 +0900

    first commit - calculator layout & CSS

---
+++ src/app.jsx
@@ -0,0 +1,32 @@
+import './app.css';
+
+function App() {
+  return (
+    <div className='calculator-grid'>
+      <div className='output'>
+        <div className='prev-operand'>123,456</div>
+        <div className='current-operand'>32644,3124,4363476,234235</div>
+      </div>
+      <button className='span-two'>AC</button>
+      <button>DEL</button>
+      <button>÷</button>
+      <button>1</button>
+      <button>2</button>
+      <button>3</button>
+      <button>*</button>
+      <button>4</button>
+      <button>5</button>
+      <button>6</button>
+      <button>+</button>
+      <button>7</button>
+      <button>8</button>
+      <button>9</button>
+      <button>-</button>
+      <button>.</button>
+      <button>0</button>
+      <button className='span-two'>=</button>
+    </div>
+  );
+}
+
+export default App;
